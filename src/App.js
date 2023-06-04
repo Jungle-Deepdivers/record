4d7d4209f049162d722b09590e931f1c6154692e
poter0501 <poter0501@gmail.com>
2023-06-04 22:38:07 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -103,10 +103,17 @@ function App() {
 
   const sendAudioToServer = (audioData) => {
     const formData = new FormData();
-    formData.append("audioFile", audioData, "audio.mp3");
+    formData.append("audio", audioData, "recording_0.wav");
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/upload", true);
+    xhr.open('POST', '/upload-audio', true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        console.log('음성 파일 전송 완료');
+      } else {
+        console.error('음성 파일 전송 실패:', xhr.status);
+      }
+    };
     xhr.send(formData);
   };
 
